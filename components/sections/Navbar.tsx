d6e10248a7d65c6bc252c08ff140327d8f3efb0b
jacob-tucker <jacobtucker818@gmail.com>
2024-05-02 15:32:44 -0700

    change design of sdk v

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -20,9 +20,9 @@ export default function Navbar() {
           >
             <div className="flex items-center gap-2 rounded-full border border-dashed border-zinc-300 py-px pl-1 pr-3 text-xs/6 font-medium text-zinc-900 dark:border-white/20 dark:text-white">
               <div className="flex h-5 w-5 items-center justify-center rounded-full bg-zinc-100 ring-1 ring-zinc-200 dark:bg-zinc-800 dark:ring-white/25">
-                <Icon icon="tabler:flask-2" />
+                v1
               </div>
-              SDK Version: v1
+              SDK Version
             </div>
           </a>
         </div>
