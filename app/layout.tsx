97bd65c6fcf5254bcd11bb0a2c463814d0e051dc
jacob-tucker <jacobtucker818@gmail.com>
2024-05-09 11:24:46 -0700

    fix: build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <StoryProvider>{children}</StoryProvider>
-        <script src="myscripts.js"></script>
       </body>
     </html>
   );
