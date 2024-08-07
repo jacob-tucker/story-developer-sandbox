66174564fa8bfa4e176f87cb1cc526d055fa9fd1
jacob-tucker <jacobtucker818@gmail.com>
2024-08-07 12:59:17 -0400

    finalize example

--- app/metadata.ts
+++
@@ -1,7 +0,0 @@
-// metadata.ts
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Developer Sandbox",
-  description: "A developer sandbox for building on Story Protocol.",
-};
