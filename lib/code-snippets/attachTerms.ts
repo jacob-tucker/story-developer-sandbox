99bbd95c1e8d6e0d402dec1b3e677395e3f46ecf
jacob-tucker <jacobtucker818@gmail.com>
2025-04-18 11:57:17 -0400

    update URI's in the code

--- lib/code-snippets/attachTerms.ts
+++ lib/code-snippets/attachTerms.ts
@@ -7,6 +7,8 @@ const MINTING_FEE: string = 10;
 const COMMERCIAL_REV_SHARE: number = 10;
 const AI_TRAINING_ALLOWED: boolean = true;
 
+// these are example terms and may not reflect
+// the exact ones you have chosen
 const licenseTerms: LicenseTerms = {
     transferable: true,
     royaltyPolicy: '0x9156e603C949481883B1d3355c6f1132D191fC41',
@@ -25,8 +27,8 @@ const licenseTerms: LicenseTerms = {
     derivativeRevCeiling: BigInt(0),
     currency: "0x1514000000000000000000000000000000000000",
     uri: AI_TRAINING_ALLOWED
-    ? "https://github.com/piplabs/pil-document/blob/998c13e6ee1d04eb817aefd1fe16dfe8be3cd7a2/off-chain-terms/CC-BY.json"
-    : "https://github.com/piplabs/pil-document/blob/ad67bb632a310d2557f8abcccd428e4c9c798db1/off-chain-terms/Default.json",
+        ? "https://raw.githubusercontent.com/piplabs/pil-document/998c13e6ee1d04eb817aefd1fe16dfe8be3cd7a2/off-chain-terms/CC-BY.json"
+        : "https://raw.githubusercontent.com/piplabs/pil-document/ad67bb632a310d2557f8abcccd428e4c9c798db1/off-chain-terms/Default.json",
 };
 
 const response = await client.ipAsset.registerPilTermsAndAttach({
